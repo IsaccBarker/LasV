bc3cee6e2b9a649612de2e452d8e0c99bcbcd3b0
Isacc Barker <milobanks@zincsoft.dev>
2022-05-30 01:39:49 -0600

    sigh

--- lasv/arch/x86_64/boot/elf.c
+++ lasv/arch/x86_64/boot/elf.c
@@ -6,15 +6,13 @@
 
 #include "elf.h"
 
-#pragma GCC push_options
-#pragma GCC optimize ("O0")
 void
-lasv_find_and_boot_elf(void)
+lasv_boot_elf(void)
 {
-    /* Elf64_Header* file_header = (Elf64_Header*) 0x8000;
-    file_header->e_type = 3; */
-    Elf64_Ehdr file_header;
-    file_header.e_type = 3;
+    // *(int *)0 = '9';
+
+    /* if (*(char*)(0x7E01) == 'E') {
+        asm volatile ("call clear_screen");
+    } */
 }
-#pragma GCC pop_options
 
