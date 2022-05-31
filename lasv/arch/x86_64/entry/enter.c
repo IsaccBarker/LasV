940f1bad8b4476ae6d3157092d0bee45df90fce7
Isacc Barker <milobanks@zincsoft.dev>
2022-05-31 00:23:14 -0600

    stage 2 is go!

--- lasv/arch/x86_64/entry/enter.c
+++
@@ -1,9 +0,0 @@
-#include "enter.h"
-#include "../shared/screen.h"
-#include "real/vga.h"
-
-void
-lasv_enter()
-{
-}
-
--- lasv/arch/x86_64/entry/enter.h
+++
@@ -1,7 +0,0 @@
-#ifndef ENTER_H
-#define ENTER_H
-
-void lasv_enter();
-
-#endif /* ENTER_H */
-
