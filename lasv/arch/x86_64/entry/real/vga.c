93c012ac37ceb7608f96f1a59a1cf0f4ca05f90d
Isacc Barker <milobanks@zincsoft.dev>
2022-05-31 00:23:14 -0600

    stage 2 is go!

--- lasv/arch/x86_64/entry/real/vga.c
+++
@@ -1,8 +0,0 @@
-#include "../../shared/vga.h"
-
-void
-real_print_string(char* str)
-{
-    // asm volatile ("");
-}
-
--- lasv/arch/x86_64/entry/real/vga.h
+++
@@ -1,7 +0,0 @@
-#ifndef REAL_VGA_H
-#define REAL_VGA_H
-
-void real_print_string(char* str);
-
-#endif /* REAL_VGA_H */
-
